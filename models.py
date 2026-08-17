482fb9dbabd17146043cbf10b2b6c54ef6fdba5e
Ignacio Pereyra Laxague <i.pereyra.laxague@itsv.edu.ar>
2026-08-17 12:54:05 -0300

    Se modificaron algunos validadores, se agregaron esEstado en EstadoReserva, y ConocerEstadoReserva y setEstado en CambioEstadoReserva

--- models.py
+++ models.py
@@ -14,16 +14,16 @@ class Chico(BaseModel):
     apellido: str = Field(..., min_length=3, max_length=30, description="Apellido del chico")
     edad: int = Field(..., gt=0, description="Edad del chico")
     tipoDocumento: TipoDocumento = Field(..., description="Tipo de documento")
-    numeroDocumento: str = Field(..., min_length=7, max_length=8, description="Numero de documento")
+    numeroDocumento: str = Field(..., min_length=7, max_length=8, pattern=r"^\d+$", description="Numero de documento")
 
 class Cliente(BaseModel):
     nombre: str = Field(..., min_length=3, max_length=30, description="Nombre del cliente")
     apellido: str = Field(..., min_length=3, max_length=30, description="Apellido del cliente")
     tipoDocumento: TipoDocumento = Field(..., description="Tipo de documento")
-    numeroDocumento: str = Field(..., min_length=7, max_length=8, description="Numero de documento")
+    numeroDocumento: str = Field(..., min_length=7, max_length=8, pattern=r"^\d+$", description="Numero de documento")
     domicilio: str = Field(..., description="Domicilio del cliente")
-    telefono: str = Field(..., min_length=8, max_length=15, description="Telefono del cliente")
-    celular: str = Field(..., min_length=8, max_length=15, description="Celular del cliente")
+    telefono: str = Field(..., min_length=8, max_length=15, pattern=r"^\d+$", description="Telefono del cliente")
+    celular: str = Field(..., min_length=8, max_length=15, pattern=r"^\d+$", description="Celular del cliente")
     email: str = Field(..., description="Email del cliente")
     hijos: list[Chico] = Field(default_factory=list, description="Hijos del cliente")
 
@@ -57,10 +57,7 @@ class Festejo(BaseModel):
     def verificar_fechas(self): 
         if self.fechaHoraFin <= self.fechaHoraInicio:
             raise ValueError("La fecha de inicio no puede ser posterior a la fecha de fin")
-        return self
 
-    @model_validator(mode="after")
-    def verificar_duracion(self):
         duracion = self.fechaHoraFin - self.fechaHoraInicio
         if duracion != timedelta(hours=2, minutes=45):
             raise ValueError("El festejo debe tener una duración de 2 horas y 45 minutos")
@@ -89,8 +86,8 @@ class Factura(BaseModel):
     numero: str = Field(..., description="Numero de factura")
     fechaHoraEmision: datetime = Field(..., description=" Fecha y hora de emision de factura")
     importeServicio: float = Field(..., gt=0, description="Importe de servicio")
-    importeExtras: float = Field(..., gt=0, description="Importe extras")
-    saldoPendiente: float = Field(..., gt=0, description="Saldo pendiente")
+    importeExtras: float = Field(..., ge=0, description="Importe extras")
+    saldoPendiente: float = Field(..., ge=0, description="Saldo pendiente")
 
     def calcularImporteTotal(self):
         return self.importeServicio + self.importeExtras
@@ -99,44 +96,94 @@ class EstadoReserva(BaseModel):
     nombre: str = Field(..., description="Nombre de estado reserva")
     descripcion: Optional[str] = Field(default=None,description="Descripcion del estado de reserva")
 
-    def getNombre(self):
-        return self.nombre
+    def esCreada(self):
+        return self.nombre == "Creada"
+
+    def esPendienteConfirmacion(self):
+        return self.nombre == "Pendiente de confirmacion"
+
+    def esAnulada(self):
+        return self.nombre == "Anulada"
+
+    def esSeniaPagada(self):
+        return self.nombre == "Senia Pagada"
+
+    def esCancelada(self):
+        return self.nombre == "Cancelada"
 
 class CambioEstadoReserva(BaseModel):
     fechaHoraInicio: datetime = Field(default_factory=datetime.now, description="Fecha y hora de inicio del cambio de estado")
     fechaHoraFin: Optional[datetime] = Field(default=None, description="Fecha y hora de fin del cambio de estado")
     estadoReserva: Optional[EstadoReserva] = Field(default=None, description="Estado asociadoa al cambio de estado reserva")
 
-    def pasaron5Dias(self):
-        tiempo_desde_cambio = datetime.now() - self.fechaHoraInicio
-        return tiempo_desde_cambio >= timedelta(days=5)
-
     def setFechaHoraFin(self):
         self.fechaHoraFin = datetime.now()
 
-    def getNombreEstado(self):
-        return self.estadoReserva.getNombre()
+    def conocerEstadosReserva(self):
+        return ESTADOS_RESERVA
 
     def setCreada(self):
-        pass
+        estados: EstadoReserva = self.conocerEstadosReserva()
+        creada = None
+        for e in estados:
+            if e.esCreada():
+                creada = e
+                break
+        if creada == None:
+            raise ValueError("No existe el estado creada")
+        else:
+            self.estadoReserva = creada
 
     def setPendienteConfirmacion(self):
-        pass
+        estados: EstadoReserva = self.conocerEstadosReserva()
+        pendiente = None
+        for e in estados:
+            if e.esPendienteConfirmacion():
+                pendiente = e
+                break
+        if pendiente == None:
+            raise ValueError("No existe el estado pendiente de confirmacion")
+        else:
+            self.estadoReserva = pendiente
 
     def pasaron5Dias(self):
-        pass
+        return self.estadoReserva.esPendienteConfirmacion() and self.fechaHoraFin is None and datetime.now() - self.fechaHoraInicio >= timedelta(days=5)
 
     def setAnulada(self):
-        pass
+        estados: EstadoReserva = self.conocerEstadosReserva()
+        anulada = None
+        for e in estados:
+            if e.esAnulada():
+                anulada = e
+                break
+        if anulada == None:
+            raise ValueError("No existe el estado anulada")
+        else:
+            self.estadoReserva = anulada
 
     def setSeniaPagada(self):
-        pass
+        estados: EstadoReserva = self.conocerEstadosReserva()
+        seniaPagada = None
+        for e in estados:
+            if e.esSeniaPagada():
+                seniaPagada = e
+                break
+        if seniaPagada == None:
+            raise ValueError("No existe el estado senia pagada")
+        else:
+            self.estadoReserva = seniaPagada
 
-    def setAnulada(self):
-        pass
-
-    def serCancelada(self):
-        pass
+    def setCancelada(self):
+        estados: EstadoReserva = self.conocerEstadosReserva()
+        cancelada = None
+        for e in estados:
+            if e.esCancelada():
+                cancelada = e
+                break
+        if cancelada == None:
+            raise ValueError("No existe el estado cancelada")
+        else:
+            self.estadoReserva = cancelada
 
 class Reserva(BaseModel):
     fechaHoraFestejo: datetime = Field(..., description="Fecha y hora seleccionada para el festejo")
@@ -181,10 +228,10 @@ class Reserva(BaseModel):
         if not self.listaCambiosEstado:
             return False
         
-        estado = self.listaCambiosEstado[-1].getNombreEstado()
-
-        if estado and estado == "PendienteDeConfirmacion" and self.listaCambiosEstado[-1].pasaron5Dias():
+        siPasaron = self.listaCambiosEstado[-1].pasaron5Dias()
+        if siPasaron:
             self.setAnulada()
+        return siPasaron
 
     def setAnulada(self):
         cambioEstado = CambioEstadoReserva()
